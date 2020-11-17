d82c3af37facf02fcc2edf6e0625ba05a27c6b18
Exodus09 <migdinism@gmail.com>
2020-11-17 00:46:45 +0000

    Update Xenon.cpp

--- Xenon/Xenon.cpp
+++ Xenon/Xenon.cpp
@@ -8,6 +8,7 @@
 #include "Loner.h"
 #include "Rusher.h"
 #include "Enemy.h"
+#include "Missile.h"
 
 int main(int argc, char* argv[])
 {
@@ -17,7 +18,11 @@ int main(int argc, char* argv[])
     Background* background = new Background();
     Spaceship* ship = new Spaceship(304, 400);
     Loner* loner = new Loner(200, 0);
-    //Enemy* enemy = new Enemy();
+
+	float position[2]{ 200.0f, 250.0f };
+	float halfSize[2]{ 4.0f, 4.0f };
+	float velocity[2]{ 0.0f, -80.0f };
+	Missile* missile = new Missile(position, halfSize, 1.0f, velocity);
 
     engine.start();
 
